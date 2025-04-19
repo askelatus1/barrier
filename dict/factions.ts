c981a97f3ad7a018439cc00b349e53d41088d390
askelatus1 <savin.roman.sergeevich@gmail.com>
2025-04-19 22:19:49 +0400

    убраны гражданские и соседи для фракций

--- dict/factions.ts
+++ dict/factions.ts
@@ -7,7 +7,6 @@ export const faction: Faction[] = [
         region: 'moldavka',
         military: true,
         terror: false,
-        neighbours: ['sin', 'civ_sin', 'civ_monolyth'],
     },
     {
         id: 'sin',
@@ -15,7 +14,6 @@ export const faction: Faction[] = [
         region:'decabristov',
         military: true,
         terror: false,
-        neighbours: ['monolith', 'civ_monolyth', 'civ_sin'],
     },
     {
         id: 'skyline',
@@ -23,32 +21,7 @@ export const faction: Faction[] = [
         region:'obrazcovo',
         military: true,
         terror: false,
-        neighbours: ['monolith', 'civ_monolyth', 'civ_skyline'],
     },
-    {
-        id: 'civ_monolyth',
-        region: 'moldavka',
-        military: false,
-        neighbours: ['sin', 'monolith', 'civ_sin'],
-        name: 'Мирные Монолит',
-        terror: false,
-    },
-    {
-        id: 'civ_sin',
-        region: 'decabristov',
-        military: false,
-        neighbours: ['monolith', 'sin', "civ_monolyth"],
-        name: 'Мирные Грех',
-        terror: false,
-    },
-    {
-        id: 'civ_skyline',
-        region: 'obrazcovo',
-        military: false,
-        neighbours: ['monolith', 'skyline', 'civ_monolyth'],
-        name: 'Мирные Скайлайн',
-        terror: false,
-    }
 ];
 
-export type FactionType = 'monolith' | 'sin' | 'skyline' | 'civ_skyline' | 'civ_monolyth' | 'civ_sin';
+export type FactionType = 'monolith' | 'sin' | 'skyline';
