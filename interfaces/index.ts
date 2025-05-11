66f5ce0d16007bd954cfa47f5b9133b923567465
Serenity <alter4444@gmail.com>
2025-05-11 17:50:25 +0400

    refactor(interfaces): удаление интерфейса Relation и обновление экспорта
    - Удалён интерфейс Relation из relation.ts для упрощения структуры.
    - Обновлён файл index.ts, исключив экспорт Relation, что улучшает читаемость и поддерживаемость кода.

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,7 +1,6 @@
 export * from './event';
 export * from './faction';
 export * from './region';
-export * from './relation';
 export * from './track';
 export * from './actorZone';
 export * from './notify';
